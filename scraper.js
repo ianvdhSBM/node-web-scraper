7b45f271603ff16df31a836239a8c6cbcda7c2b4
Ian Van Den Heuvel <ianvdh@gmail.com>
2016-02-16 18:07:06 +0000

    Add each loop to access permissions and link from each table row

--- scraper.js
+++ scraper.js
@@ -1,12 +1,21 @@
 var request = require('request');
-var cheerio = require('cheerio');
+var cheerio = require('cheerio')
 
 var url = process.argv[2];
 
-request(url, function(error, response, body) {
+var rows = [];
+
+request.get(url, function(error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(body)
+    var cheerio = require('cheerio'),
+      $ = cheerio.load(body);
+    // console.log($('td code').text());
+    $('tr').slice(32, 40).each (function() {
+      var permissions = $('code', this).first().text();
+      var link = $('a', this).attr('href');
+      console.log(permissions);
+      console.log(link);
+    })
+
   }
-  // console.log(response.statusCode);
-  // console.log(response.headers['content-type']);
-})
+});
